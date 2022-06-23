e01b534d0d8ff3c63e70e061a52294b9300bff00
RJRP44 <romainpouvreau@orange.fr>
2022-06-23 14:46:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+<h1 align="center"> Ouchat Electronics </h1>
+<p align="center">
+<img align="center" src="https://ouchat.fr/ouchat.svg" alt="ouchat logo" width="150">
+</p>
+
+<p align="center"><img src="https://img.shields.io/badge/version-6.0.0-blue" alt="version">
+<img src="https://img.shields.io/badge/framework-esp--idf-lightgrey" alt="platform">
+<img src="https://img.shields.io/github/license/RJRP44/Ouchat-Electronics" alt="license">
+</p>
+
+
+A project using [vl53l5cx](https://www.st.com/en/imaging-and-photonics-solutions/vl53l5cx.html) to detect the passage of the cat to the outside.
+
+> **Warning**
+> This Project **only** works on ESP32D with at least 8MB of flash size.
+
+## 📌 Contents
+
+* [Structure](#structure)
+
+---
+
+
+### Structure
+
+```
+├── CMakeLists.txt
+├── 📁 components /                
+│   └── 📁 Vl53l5cx /
+│      └── 📁 include /
+│          ├── platform.h
+│          ├── vl53v5cx_api.h
+│          └── vl53v5cx_buffer.h
+├── 📁 main /
+│   ├── CMakeLists.txt
+│   ├── main.c
+└── README.md  
+```
+---
+
+## 📝 License
+
+Copyright © 2022 [RJRP44](https://www.github.com/RJRP44).
+
+This project is [BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause/)  licensed.
+
+## ✨ Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 👤 Authors
+
+- [@RJRP44](https://www.github.com/RJRP44)
+
---
+++ main/main.c
@@ -0,0 +1,108 @@
+#include <sys/cdefs.h>
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+#include "../components/vl53l5cx/include/vl53l5cx_api.h"
+
+static const char *TAG = "v53l5cx_lib";
+
+#define I2C_SDA_NUM                      21
+#define I2C_SCL_NUM                      22
+#define I2C_CLK_SPEED                    1000000
+#define I2C_TIMEOUT                      400000
+#define I2C_TX_BUF                       0
+#define I2C_RX_BUF                       0
+
+static esp_err_t i2c_master_init(void) {
+
+    i2c_config_t conf = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = I2C_SDA_NUM,
+            .scl_io_num = I2C_SCL_NUM,
+            .sda_pullup_en = GPIO_PULLUP_ENABLE,
+            .scl_pullup_en = GPIO_PULLUP_ENABLE,
+            .master.clk_speed = I2C_CLK_SPEED,
+    };
+
+    i2c_param_config(I2C_NUM_1, &conf);
+    i2c_set_timeout(I2C_NUM_1, I2C_TIMEOUT);
+
+    return i2c_driver_install(I2C_NUM_1, conf.mode, I2C_RX_BUF, I2C_TX_BUF, 0);
+}
+
+_Noreturn void app_main(void) {
+
+    //Initialize the i2c bus
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
+
+    uint8_t status, isAlive, i , isReady;
+
+    //Define the i2c address and port
+    VL53L5CX_Configuration *configuration = malloc(sizeof *configuration);
+    configuration->platform.port = I2C_NUM_1;
+    configuration->platform.address = 0x52;
+
+    //Wakeup the sensor
+    status = vl53l5cx_is_alive(configuration, &isAlive);
+    if (!isAlive || status) {
+        printf("VL53L5CX not detected at requested address\n");
+        return;
+    }
+
+    /* (Mandatory) Init VL53L5CX sensor */
+    status = vl53l5cx_init(configuration);
+    if (status) {
+        printf("VL53L5CX ULD Loading failed\n");
+        return;
+    }
+
+    printf("VL53L5CX ULD ready ! (Version : %s)\n", VL53L5CX_API_REVISION);
+
+    status = vl53l5cx_set_resolution(configuration, VL53L5CX_RESOLUTION_8X8);
+    if (status) {
+        printf("vl53l5cx_set_resolution failed, status %u\n", status);
+        return;
+    }
+
+    status = vl53l5cx_set_ranging_frequency_hz(configuration, 15);
+    if(status)
+    {
+        printf("vl53l5cx_set_ranging_frequency_hz failed, status %u\n", status);
+        return;
+    }
+
+    status = vl53l5cx_start_ranging(configuration);
+
+    VL53L5CX_ResultsData Results;
+
+    while (true) {
+        /* Use polling function to know when a new measurement is ready.
+         * Another way can be to wait for HW interrupt raised on PIN A3
+         * (GPIO 1) when a new measurement is ready */
+
+        status = vl53l5cx_check_data_ready(configuration, &isReady);
+
+        if (isReady) {
+            vl53l5cx_get_ranging_data(configuration, &Results);
+
+            /* As the sensor is set in 8x8 mode, we have a total
+             * of 64 zones to print. For this example, only the data of
+             * first zone are print */
+            printf("Print data no : %3u\n", configuration->streamcount);
+            for (i = 0; i < 64; i++) {
+                printf("Zone : %3d, Status : %3u, Distance : %4d mm\n",
+                       i,
+                       Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE * i],
+                       Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE * i]);
+            }
+            printf("\n");
+        }
+
+        /* Wait a few ms to avoid too high polling (function in platform
+         * file, not in API) */
+        WaitMs(&(configuration->platform), 5);
+    }
+
+}
